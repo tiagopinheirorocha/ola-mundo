85a3065ff5c256ed66d1729c77a30eea22f4dc0a
tiagopinheirorocha <161031251+tiagopinheirorocha@users.noreply.github.com>
2024-03-01 10:32:12 -0300

    Create code.js

---
+++ code.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const texto = document.body.querySelector("#texto");
+    texto.addEventListener("click", () => {
+        if (texto.textContent==="Olá Mundo")
+           texto.textContent="Hello World";
+        else
+            texto.textContent="Olá Mundo"   
+
+    });    
+});
